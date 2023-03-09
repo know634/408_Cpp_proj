237daec074edfa8ebc0eca1e56368587e60b67ff
根荣 许 <1589575776@qq.com>
2023-03-09 17:05:20 +0800

    输出时间

--- 408_C++_proj/hello_world.cpp
+++ 408_C++_proj/hello_world.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello, World!一二三";
+    cout << "Hello, World!一二三\n";
+    //输出时间
+    cout << "The time is " << __TIME__ << endl;
+
     return 0;
 }
